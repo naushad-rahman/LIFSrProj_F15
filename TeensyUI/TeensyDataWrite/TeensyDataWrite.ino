83ba6a6cc4445fc0c11a1ee3abf79eceafde353f
Sam Graham <samgraham31@gmail.com>
2015-05-29 09:51:55 -0600

    Files for teensy UI

---
+++ TeensyUI/TeensyDataWrite/TeensyDataWrite.ino
@@ -0,0 +1,53 @@
+/*
+When we run this program we will read in voltage values
+from pins A0(PMT) and A1(Peak Detector) from a Teensy and write
+these values, along with the time in microseconds from the start 
+of the program, to the serial port for retrieval by our python
+program, Teensy Data.
+*/
+
+int pmt;
+int pd;
+int time;
+IntervalTimer timer0;
+
+//Function we will run in response to a 100 us timer
+void SampleVoltage() {
+  pmt = analogRead(0); //Voltage read from pin A0 of the Teensy
+  pd = analogRead(1); //Voltage read from pin A1 of the Teensy
+  time = micros(); //Time in microseconds since start of program
+  //We will write our values as an array of bytes for easy
+  //transmission and retrieval
+  unsigned char serialBytes[8];
+  serialBytes[0] = (time >> 24) & 0xff;
+  serialBytes[1] = (time >> 16) & 0xff;
+  serialBytes[2] = (time >> 8) & 0xff;
+  serialBytes[3] = time & 0xff;
+  serialBytes[4] = (pmt >> 8) & 0xff;
+  serialBytes[5] = pmt & 0xff;
+  serialBytes[6] = (pd >> 8) & 0xff;
+  serialBytes[7] = pd & 0xff;
+  Serial.write(serialBytes,8);
+}
+
+void timer_setup() {
+  timer0.begin(SampleVoltage, 100);  //SampleVoltage will run every 100 us
+}
+  
+void timer_stop() {
+  timer0.end();
+}
+
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(2000); //To allow time for serial port to begin
+  timer_setup(); //Run timer
+}
+
+void loop()
+{
+
+}
+
