43bf399ca47fde795977e6c6cdad187b333b29a3
gregnordin <nordin@byu.edu>
2015-07-10 17:55:44 -0600

    Establish simple serial communication from Teensy to python code in a notebook

--- DSP/incremental_development/a_python_teensy_com/a_python_teensy_com.ino
+++ DSP/incremental_development/a_python_teensy_com/a_python_teensy_com.ino
@@ -1,22 +1,19 @@
 /*
-  AnalogReadSerial
-  Reads an analog input on pin 0, prints the result to the serial monitor.
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
-
- This example code is in the public domain.
- */
+  Send sequence of numbers (1 to 100) over serial port. Repeat indefinitely.
+*/
 
 // the setup routine runs once when you press reset:
+int counter;
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+  counter = 1;
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // print out the value you read:
-  Serial.println(sensorValue);
-  delay(1);        // delay in between reads for stability
+  Serial.println(counter);
+  counter++;
+  if (counter==101) counter=1;
+  delay(1000);        // delay in ms between reads for stability
 }
