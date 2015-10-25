1a3ba6168f2a433b3cab68e7df6e74462d55cb2f
Jared Hunsaker <jarhun@byu.net>
2015-10-24 21:56:37 -0600

    Serial communication is (mostly) fixed!

--- TeensyUI/TeensyDataWrite/TeensyDataWrite.ino
+++ TeensyUI/TeensyDataWrite/TeensyDataWrite.ino
@@ -6,8 +6,7 @@ of the program, to the serial port for retrieval by our python
 program, Teensy Data.
 */
 
-int pmt;
-int pd;
+int pmt = 0;
 unsigned long time;
 IntervalTimer timer0;
 
@@ -18,7 +17,7 @@ void SampleVoltage() {
   time = micros(); //Time in microseconds since start of program
   //We will write our values as an array of bytes for easy
   //transmission and retrieval
-  unsigned char serialBytes[6];
+  unsigned char serialBytes[8];
   serialBytes[0] = (time >> 24) & 0xff;
   serialBytes[1] = (time >> 16) & 0xff;
   serialBytes[2] = (time >> 8) & 0xff;
@@ -26,20 +25,27 @@ void SampleVoltage() {
   serialBytes[4] = (pmt >> 8) & 0xff;
   serialBytes[5] = pmt & 0xff;
 
-//  serialBytes[0] = 0;
-//  serialBytes[1] = 0;
-//  serialBytes[2] = 0;
-//  serialBytes[3] = 0;
-//  serialBytes[4] = 0;
-//  serialBytes[5] = 0;
-//  serialBytes[6] = 0;
-//  serialBytes[7] = 0;
+  //These two bytes are here because the serial communication
+  //doesn't work when only 6 bytes are sent.
+  serialBytes[6] = 0x00;
+  serialBytes[7] = 0x00;
+
+//  serialBytes[0] = 0x02;
+//  serialBytes[1] = 0x04;
+//  serialBytes[2] = 0x08;
+//  serialBytes[3] = 0x10;
+//  serialBytes[4] = 0x20;
+//  serialBytes[5] = 0x40;
+//  serialBytes[6] = 0x00;
+//  serialBytes[7] = 0x00;
   
-  Serial.write(serialBytes,6);
+  Serial.write(serialBytes,8);
+
+  //pmt += 10;
 }
 
 void timer_setup() {
-  timer0.begin(SampleVoltage, 100);  //SampleVoltage will run every 100 us
+  timer0.begin(SampleVoltage, 110);  //SampleVoltage will run every 100 us
 }
   
 void timer_stop() {
